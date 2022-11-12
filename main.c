51b31fc58a65ebdaec8142b35d9dd8d54263dacd
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2022-11-12 23:32:10 +0100

    Moved shooting to function, added some comments

--- main.c
+++ main.c
@@ -17,6 +17,13 @@ void doDraw(int mapUpper,
 	    int playerRadius,
 	    Projectile projectiles[],
 	    int maxProjectiles) {
+  /*
+    Helper function to (re)draw everything, in the following order
+    - background
+    - map
+    - player
+    - projectiles
+   */
   BeginDrawing();
   ClearBackground(RAYWHITE);      
   DrawRectangle(mapUpper, mapLeft, mapWidth, mapHeight, RED);
@@ -28,9 +35,27 @@ void doDraw(int mapUpper,
   EndDrawing();  
 }
 
+void shoot(float xSpeed,
+	   float ySpeed,
+	   Vector2 origin,
+	   Projectile projectiles[],
+	   int* projectileAddIdx,
+	   int maxProjectiles) {
+  /*
+    Register a new projectile
+   */
+  Projectile* p = &projectiles[*projectileAddIdx];
+  p->position = origin;
+  p->speed = (Vector2) {xSpeed, ySpeed};
+  p->radius = 5;
+  p->lifeTime = 60;	
+  p->enabled = 1;
+  *projectileAddIdx = (*projectileAddIdx + 1) % maxProjectiles;	
+}
+
 
 int main(void) {
-  // init vals
+  // init map values
   const int screenWidth  = 800;
   const int screenHeight = 450;
   const int mapWidth     = screenWidth -  20;
@@ -40,69 +65,48 @@ int main(void) {
   const int mapLeft      = 10;
   const int mapRight     = mapLeft + mapWidth;
 
-  InitWindow(screenWidth, screenHeight, "Sprutte Game");
-  SetTargetFPS(60);
-  ClearBackground(RAYWHITE);
-
+  // init player values
   Vector2 playerPos       = { (float)screenWidth/2, (float)screenHeight/2 };
   const int playerRadius  = 20;
   const float playerSpeed = 2.0f;
 
+  // init projectile values
   const int maxProjectiles = 50;
   int projectileAddIdx = 0;  
   Projectile projectiles[maxProjectiles];
   for (int i = 0; i < maxProjectiles; i++) {
     projectiles[i] = (Projectile){(Vector2){0,0}, (Vector2){0,0}, 0, 0, 0};
   }
-  
+
+  // set up raylib
+  InitWindow(screenWidth, screenHeight, "Sprutte Game");
+  SetTargetFPS(60);
   
   // Main game loop
   while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-      // controls
+      // Player movement
       if (IsKeyDown(KEY_D) && playerPos.x + playerRadius <= mapRight) playerPos.x += playerSpeed;
       if (IsKeyDown(KEY_A) && playerPos.x - playerRadius >= mapLeft)  playerPos.x -= playerSpeed;
       if (IsKeyDown(KEY_S) && playerPos.y + playerRadius <= mapLower) playerPos.y += playerSpeed;
       if (IsKeyDown(KEY_W) && playerPos.y - playerRadius >= mapUpper) playerPos.y -= playerSpeed;
 
+      // Detect shooting, register new projectile
       if (IsKeyDown(KEY_RIGHT)) {
-	Projectile* p = &projectiles[projectileAddIdx];
-	p->position = playerPos;
-	p->speed = (Vector2) { (float)5, (float)0 };
-	p->radius = 5;
-	p->lifeTime = 60;	
-	p->enabled = 1;
-	projectileAddIdx = (projectileAddIdx + 1) % maxProjectiles;
+	shoot(5.0f, 0.0f, playerPos, projectiles, &projectileAddIdx, maxProjectiles);
       } else if (IsKeyDown(KEY_LEFT)) {
-	Projectile* p = &projectiles[projectileAddIdx];
-	p->position = playerPos;
-	p->speed = (Vector2) { (float)-5, (float)0 };
-	p->radius = 5;
-	p->lifeTime = 60;	
-	p->enabled = 1;
-	projectileAddIdx = (projectileAddIdx + 1) % maxProjectiles;
+	shoot(-5.0f, 0.0f, playerPos, projectiles, &projectileAddIdx, maxProjectiles);
       } else if (IsKeyDown(KEY_DOWN)) {
-	Projectile* p = &projectiles[projectileAddIdx];
-	p->position = playerPos;
-	p->speed = (Vector2) { (float)0, (float)5 };
-	p->radius = 5;
-	p->lifeTime = 60;	
-	p->enabled = 1;
-	projectileAddIdx = (projectileAddIdx + 1) % maxProjectiles;
+	shoot(0.0f, 5.0f, playerPos, projectiles, &projectileAddIdx, maxProjectiles);
       } else if (IsKeyDown(KEY_UP)) {
-	Projectile* p = &projectiles[projectileAddIdx];
-	p->position = playerPos;
-	p->speed = (Vector2) { (float)0, (float)-5 };
-	p->radius = 5;
-	p->lifeTime = 60;	
-	p->enabled = 1;
-	projectileAddIdx = (projectileAddIdx + 1) % maxProjectiles;
+	shoot(0.0f, -5.0f, playerPos, projectiles, &projectileAddIdx, maxProjectiles);
       }      
 
+      // Update each projectile 
       for (int i = 0; i < maxProjectiles; i++) {
 	Projectile* p = &projectiles[i];
 	if (p->enabled) {
-	  if (p->lifeTime == 0) {
+	  if (p->lifeTime == 0) { // disable if lifetime ran out
 	    p->enabled = 0;
 	    continue;
 	  }
@@ -112,6 +116,7 @@ int main(void) {
 	}
       }      
 
+      // draw everything
       doDraw(mapUpper,
 	     mapLeft,
 	     mapWidth,
